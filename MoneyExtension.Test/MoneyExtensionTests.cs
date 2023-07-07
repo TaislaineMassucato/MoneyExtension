7c74d19777024ecb9a63541dc021b77d0673c9c3
Taislaine Massucato <massucato2@outlook.com>
2023-07-07 10:53:16 -0300

    Finalizado a primeira versão

---
+++ MoneyExtension.Test/MoneyExtensionTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyExtension.Test;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()                                                    
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
